6fea759a4aa093ef9664cc4ac25c0e498c8e50ec
262667641@qq.com <262667641@qq.com>
2019-08-14 21:11:16 +0800

    update all

--- interface/app-interface/record_add_test.py
+++ interface/app-interface/record_add_test.py
@@ -2,9 +2,10 @@ import requests
 import unittest
 from Global_base import global_base
 from parameterized import parameterized
+from Global_base import login
 
 
-class SendPhoneCode(unittest.TestCase):
+class RecordAdd(unittest.TestCase):
     def setUp(self):
         self.url = global_base.DefTool.url(self, '/userrecord/record/add.do')
 
@@ -12,14 +13,14 @@ class SendPhoneCode(unittest.TestCase):
         print(self.result)
 
     @parameterized.expand([
-        ("找回密码成功", "867910035562539", "1003", "1", "154907325559300096", "sinaif",
+        ("浏览记录上报", "867910035562539", "1003", "1", "154907325559300096", "sinaif",
          "1", "2.6.0", "15", "1234")
     ])
-    def test_send_phone_code(self, case, deviceId, productId, deviceType, targetId, channelId, actType,
+    def test_record_add(self, case, deviceId, productId, deviceType, targetId, channelId, actType,
                              versionName, versionNo, actSource):
-
-        accountId = "18888888888"
-        token = "token"
+        values = login.Login.test_login(self,18888888888)
+        accountId = values[1]
+        token = values[2]
         code = "1234512dd"
         pa = {"targetId": targetId, "code": code, "accountId": accountId, "deviceId": deviceId, "actType": actType, "deviceType": deviceType,
               "productId": productId, "channelId": channelId, "versionName": versionName, "versionNo": versionNo, "actSource": actSource, "token": token}
